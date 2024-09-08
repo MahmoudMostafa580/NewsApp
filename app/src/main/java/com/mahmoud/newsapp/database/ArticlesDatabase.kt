bc0ba1ea84dc2f196a9f6ffd3623c0c16cfcc6a5
Mahmoud Mostafa <mahmoud.mostafa20112011@gmail.com>
2024-09-08 20:28:59 +0300

    Create converter class for database

--- app/src/main/java/com/mahmoud/newsapp/database/ArticlesDatabase.kt
+++ app/src/main/java/com/mahmoud/newsapp/database/ArticlesDatabase.kt
@@ -4,9 +4,11 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.mahmoud.newsapp.models.Article
 
 @Database(entities = [Article::class], version = 1)
+@TypeConverters(Converters::class)
 abstract class ArticlesDatabase : RoomDatabase() {
 
     companion object {

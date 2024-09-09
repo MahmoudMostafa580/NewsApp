ffbff8320802365f6b921ac93d8667a8dd6f1b26
Mahmoud Mostafa <mahmoud.mostafa20112011@gmail.com>
2024-09-09 12:29:49 +0300

    Create viewmodel

---
+++ app/src/main/java/com/mahmoud/newsapp/repository/NewsRepository.kt
@@ -0,0 +1,9 @@
+package com.mahmoud.newsapp.repository
+
+import com.mahmoud.newsapp.database.ArticlesDatabase
+
+class NewsRepository(
+    db: ArticlesDatabase
+) {
+
+}
---
+++ app/src/main/java/com/mahmoud/newsapp/ui/NewsViewModel.kt
@@ -0,0 +1,9 @@
+package com.mahmoud.newsapp.ui
+
+import androidx.lifecycle.ViewModel
+import com.mahmoud.newsapp.repository.NewsRepository
+
+class NewsViewModel(val newsRepository: NewsRepository): ViewModel() {
+
+
+}

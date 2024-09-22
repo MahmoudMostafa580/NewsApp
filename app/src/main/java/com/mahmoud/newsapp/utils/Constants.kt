97a30167f9dbc0db707f194e138d9fcf70c7e98f
Mahmoud Mostafa <mahmoud.mostafa20112011@gmail.com>
2024-09-22 09:45:45 +0300

    Finish Design

--- app/src/main/java/com/mahmoud/newsapp/utils/Constants.kt
+++ app/src/main/java/com/mahmoud/newsapp/utils/Constants.kt
@@ -2,7 +2,7 @@ package com.mahmoud.newsapp.utils
 
 class Constants {
     companion object{
-        const val API_KEY = "ba03176948e04521858368f1703db7b6"
+        const val API_KEY = "f6a0140b464d46ab950a52c669795e7b"
         const val BASE_URL = "https://newsapi.org"
         const val SEARCH_NEWS_TIME_DELAY = 500L
     }
